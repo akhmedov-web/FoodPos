c01fefeb07e43c9ce3eb047b395550ece1fb637d
Utkirov Boburbek <boburbekutkirov10@gmail.com>
2022-07-08 19:03:29 +0500

    v4

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 export default function Certificate() {
   return (
-    <div className='text-light'>
-      Certificate
-      </div>
+
+    <div className='text-light' >
+
+    Certificate
+
+    </div>
   )
 }
