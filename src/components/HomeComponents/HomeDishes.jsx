76837aa384c3c7115a06f992564ec3e6d7aca732
Akhmedov Web <ahmedovshohbaxt@gmail.com>
2022-07-08 18:51:54 +0500

    v3

--- src/components/HomeComponents/HomeDishes.jsx
+++ src/components/HomeComponents/HomeDishes.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import "../../style/Home.css";
+import HotDishCard from './HotDishCard';
 export default function HomeDishes() {
   return (
-    <div>HomeDishes</div>
+    <div className='hotDishes'>
+      <ul className='hotDishesHeader'>
+        <h4>CHOOSE DISHES</h4>
+  <select class="form-select text-white me-4 " style={{backgroundColor:"#1F1D2B"}} aria-label="Default select example">
+    <option selected>Dine in</option>
+    <option value="1">Breakfast</option>
+    <option value="2">Dinner</option>
+    <option value="3">Supper</option>
+  </select>
+      </ul>
+      <div className='hotDishesCardArea'>
+<HotDishCard/>
+<HotDishCard/>
+<HotDishCard/>
+<HotDishCard/>
+<HotDishCard/>
+<HotDishCard/>
+      </div>
+    </div>
   )
 }
